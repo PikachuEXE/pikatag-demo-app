3380895ade35e43360918b511be758402116a791
PikachuEXE <pikachuexe@gmail.com>
2013-02-13 11:15:50 +0800

    + Add methods to Item for tag list handling

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,4 +3,12 @@ class Item < ActiveRecord::Base
 
   has_many :taggings, as: :taggable
   has_many :tags, through: :taggings
+
+  def tag_list
+    tags.to_tag_list
+  end
+
+  def tag_list=(tag_list_str)
+    self.tags = Tag.from_tag_list(tag_list_str)
+  end
 end
