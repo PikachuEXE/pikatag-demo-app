442ab844fd067c6e09a158e4c5d6b695b0543720
PikachuEXE <pikachuexe@gmail.com>
2013-02-13 11:01:35 +0800

    + Add some class methods to handle "tag list" (String with tag names separated by commas)

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -2,4 +2,24 @@ class Tag < ActiveRecord::Base
   attr_accessible :name
 
   validates_presence_of :name
+  validates_uniqueness_of :name
+
+  SEPERATOR = ','
+
+  def self.tag_list
+    pluck(:name).join(SEPERATOR)
+  end
+
+  def self.tag_list=(tag_list_str)
+    tag_names = tag_list_str.split(SEPERATOR).reject(&:blank?)
+    tag_names.collect do |tag_name|
+      find_or_create_by_name(tag_name)
+    end
+  end
+
+  private
+
+  def self.find_or_create_by_name(name)
+    where(name: name).first || create!(name: name)
+  end
 end
