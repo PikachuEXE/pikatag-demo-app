168d79158d75f32e671e8b30d89bac7430d4b501
PikachuEXE <pikachuexe@gmail.com>
2013-02-13 09:03:00 +0800

    + Create Rails 3.2.11 blank app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PikatagDemoApp::Application.config.secret_token = '81cef78221d4e42c9bfbead59be8a2b3d0bd62ccda565b3ce7b2be7b3a5ae791f420abd80069cb65068c1b34f099a341e6dc6ccdf5863d83f2b08ae9009de379'
