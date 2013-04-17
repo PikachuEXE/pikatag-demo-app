059a1e23109c33999fa3ceacefe4a10f5cac2c0b
PikachuEXE <pikachuexe@gmail.com>
2013-04-17 12:18:36 +0800

    * Use strong parameter approach in controllers

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -8,12 +8,18 @@ class ItemsController < ApplicationController
   end
 
   def create
-    @item = Item.new(params[:item])
+    @item = Item.new(resource_params)
 
     if @item.save
       redirect_to items_path
     else
       render 'new'
     end
+  end
+
+  private
+
+  def resource_params
+    params.require(:item).permit(:name)
   end
 end
